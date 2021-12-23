4f8646996ebef24a514c79b91804c3b94074583e
김진근 <freedom7113@gmail.com>
2021-12-23 18:35:19 +0900

    fix error

--- src/js/ApiController.js
+++ src/js/ApiController.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-import { useHistory } from "react-router";
 
-const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
 const instance = axios.create();
-const isRefreshing = false;
 
 instance.interceptors.request.use(
   function (config) {
@@ -21,8 +18,6 @@ instance.interceptors.response.use(
   },
 
   async (error) => {
-    // const { push } = useHistory();
-
     const {
       config,
       response: { status },
@@ -34,7 +29,6 @@ instance.interceptors.response.use(
     if (refreshToken === null) {
       //리프레쉬 토큰이 없으면
       window.location.href = "/login";
-      // push("/login");
       return Promise.reject(error);
     }
 
@@ -44,7 +38,7 @@ instance.interceptors.response.use(
     };
 
     try {
-      const response = await axios.put(`${PROXY}/api/auth`, null, {
+      const response = await axios.put(`/api/auth`, null, {
         //헤더에 리프레쉬 토큰을 담음
         headers: header,
       });
@@ -87,68 +81,11 @@ instance.interceptors.response.use(
         // 리프레쉬 실패 리프레쉬 토큰 만료 실패
         instance.defaults.headers.common["Authorization"] = null;
         localStorage.removeItem("refreshToken");
-        // push("/login");
+
         window.location.href = "/login";
         return Promise.reject(error);
       }
     }
-    // (error) => {
-    //   const {
-    //     config,
-    //     response: { status },
-    //   } = error;
-    //   // if (status !== 403) {
-    //   //   return Promise.reject(error);
-    //   // }
-    //   console.log("A");
-    //   const refreshToken = localStorage.getItem("refreshToken");
-
-    //   const originalRequest = config;
-    //   if (refreshToken === null) {
-    //     //리프레쉬 토큰이 없으면
-    //     window.location.href = "/login";
-    //     return Promise.reject(error);
-    //   }
-
-    //   //리프레쉬 토큰이 있다.
-    //   const header = {
-    //     "X-Refresh-Token": refreshToken,
-    //   };
-    //   const response = axios
-    //     .put("api/auth", null, {
-    //       //헤더에 리프레쉬 토큰을 담음
-    //       headers: header,
-    //     })
-    //     .then((response) => {
-    //       console.log("B");
-    //       //토큰 리프레쉬 함 성공
-    //       const { accessToken, refreshToken } = response.data;
-
-    //       instance.defaults.headers.common["Authorization"] = accessToken;
-    //       localStorage.setItem("refreshToken", refreshToken);
-
-    //       originalRequest.headers = { Authorization: accessToken };
-    //       const newResponse = axios(originalRequest)
-    //         .then((response) => {
-    //           console.log("C");
-    //           return Promise.resolve(response);
-    //         })
-    //         .catch((error) => {
-    //           console.log("D");
-    //           return Promise.reject(error);
-    //         });
-    //       return newResponse;
-    //     })
-    //     .catch((error) => {
-    //       //리프레쉬 실패 리프레쉬 토큰 만료 실패
-    //       console.log("E");
-    //       instance.defaults.headers.common["Authorization"] = null;
-    //       localStorage.removeItem("refreshToken");
-    //       window.location.href = "/login";
-    //       return Promise.reject(error);
-    //     });
-    //   return response;
-    // }
   }
 );
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,4 @@
 const createProxyMiddleware = require("http-proxy-middleware");
-const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
 
 module.exports = function (app) {
   app.use(
@@ -7,7 +6,7 @@ module.exports = function (app) {
       target: "http://220.90.237.33:7070",
       changeOrigin: true,
       pathRewrite: {
-        "^/api": `${PROXY}`,
+        "^/api": "",
       },
     })
   );
