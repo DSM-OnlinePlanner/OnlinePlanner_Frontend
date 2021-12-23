d7fd07e7a19450e154b74102ea462762a138ae8d
김진근 <freedom7113@gmail.com>
2021-12-23 18:51:37 +0900

    change render

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import axios from 'axios'
-import {RefreshToken} from './js/Token.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import { RefreshToken } from "./js/Token.js";
 
 axios.defaults.withCredentials = true;
 
-RefreshToken(() => { //토큰을 리프레쉬 한 다음 렌더링 함
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-})
+// RefreshToken(() => { //토큰을 리프레쉬 한 다음 렌더링 함
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// })
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
