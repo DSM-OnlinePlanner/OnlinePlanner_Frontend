6bff68cd18a938db3bdbbc85246a4393c367f0a2
김진근 <freedom7113@gmail.com>
2021-12-23 18:47:46 +0900

    remove file

--- online_planner/src/component/style/globalStyle.js
+++
@@ -1,9 +0,0 @@
-import styled, { css } from "styled-components";
-const medium = 500;
-const regular = 400;
-
-export const SectionTitle = styled.div`
-    font-size: 24px;
-    font-weight: ${medium};
-    margin-bottom: 10px;
-`;
