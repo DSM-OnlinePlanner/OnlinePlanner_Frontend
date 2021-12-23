9cff54b7b3e50ed7e932e25e1b24daacc3adca90
김진근 <freedom7113@gmail.com>
2021-12-23 22:40:13 +0900

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
-const createProxyMiddleware = require("http-proxy-middleware");
+// const createProxyMiddleware = require("http-proxy-middleware");
 
-module.exports = function (app) {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: "http://220.90.237.33:7070",
-      changeOrigin: true,
-      pathRewrite: {
-        "^/api": "",
-      },
-    })
-  );
-};
+// module.exports = function (app) {
+//   app.use(
+//     createProxyMiddleware("/api", {
+//       target: "http://220.90.237.33:7070",
+//       changeOrigin: true,
+//       pathRewrite: {
+//         "^/api": "",
+//       },
+//     })
+//   );
+// };
